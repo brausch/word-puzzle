9c2026fe54a66c628283c2d30d41abc9193bbd61
Brittany Rausch <berausch247@gmail.com>
2016-03-29 15:47:53 -0700

    Chnage the append to a variable for the output

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,15 @@ $(function(){
   $("#number-input").submit(function(event){
     event.preventDefault();
     var sentence = $("#sentence").val();
+    var noVowels = ""
       $(".output").show();
     for (var index = 0; index < sentence.length; index += 1) {
-      if(sentence[index] === "a" || sentence[index] === "e" || sentence[index] === "i" || sentence[index] === "o" || sentence[index] === "u"){
-        $(".output").append("-");
+      if(sentence[index].toLowerCase() === "a" || sentence[index].toLowerCase() === "e" || sentence[index].toLowerCase() === "i" || sentence[index].toLowerCase() === "o" || sentence[index].toLowerCase() === "u"){
+        noVowels = noVowels + "-";
       } else {
-        $(".output").append(sentence[index]);}
+        noVowels = noVowels + sentence[index] ;
     }
+    $(".output").text(noVowels);
+  }
   });
 });
